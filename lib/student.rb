b3ffbb44b28222ae1ef24d78a38372a853bae843
William Pannell <lpann5@gmail.com>
2020-07-01 22:35:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -73,6 +73,17 @@ class Student
     end
   end
 
+  def self.students_below_12th_grade
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE students.grade < 12
+    SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
   def self.first_X_students_in_grade_10(num_students)
     sql = <<-SQL
     SELECT *
